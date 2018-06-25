1efe74dc3a15f3c7b06620694987d1cc0f535fff
Paia Thao <paiathao09@gmail.com>
2018-06-25 13:48:11 -0500

    btn click

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ function readyNow () {
     $ ('#generateBtn').on('click', addDiv);
 }
 
+sum = 0;
+
 function addDiv () {
     console.log('btn click');
+    $('#bodyDiv').append( [
+        '<div><p id="numberClick">' + 'Number of times Generate button is press: ' + sum++ + '</p></div>',
+        '<button id="swapBtn">Swap</button>',
+        '<button id="deleteBtn">Delete</button>',
+    ])
 }
+
